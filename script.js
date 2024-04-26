6a00be6ad1faadd2923e0c4678db3ab76d597f8f
MiriamPecora <miriampecora97@gmail.com>
2024-04-26 15:49:23 +0200

    [COMPLETED] Completed task

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const userAge = parseInt(prompt("Inserisci la tua età"));
 const kmToTravel = parseInt(prompt("Inserisci i km da percorrere"));
 const kmPrice = 0.21;
 
+document.getElementById("age").innerHTML += userAge;
+document.getElementById("kms").innerHTML += kmToTravel;
 console.log (userAge, "questa è la tua età")
 console.log(kmToTravel, "questi sono i km che vuoi percorrere")
 
@@ -15,15 +17,16 @@ if (userAge < 18){
     discount = totalPrice * 20 / 100;
     console.log(discount, "questo è lo sconto applicato ai minorenni");
     totalPrice = totalPrice - discount;
+    document.getElementById("discount").innerHTML = `Questo è lo sconto che verrà applicato €${discount}`;
 }
 else if (userAge >= 65){
     discount = totalPrice * 40 / 100;
     console.log(discount, "questo è lo sconto applicato agli over 65");
     totalPrice = totalPrice - discount;
+    document.getElementById("discount").innerHTML = `Questo è lo sconto che verrà applicato €${discount}`;
 }
 
 totalPrice = Math.round(totalPrice * 100) / 100;
 
-console.log(totalPrice, "questo è il prezzo totale da pagare (con sconti applicati)")
-
-
+document.getElementById("total_price").innerHTML += totalPrice;
+console.log(totalPrice, "questo è il prezzo totale da pagare (con sconti applicati)");
